8c900e7c7e878cfc3f57dfd64caa2b8e2fd7c1b1
Munmud <moontasir042@gmail.com>
2020-09-02 10:48:18 +0600

    router added

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const app = express()
 
+const router = require('./router')
+
 let port = process.env.PORT
 if (port == null || port =="") port = 3000
 
 app.use(express.static('public'))
 app.set('views' , 'views')
 app.set('view engine','ejs')
-
-app.get('/' , (req , res) => {
-    res.render('home-guest')
-})
+app.use('/' , router)
 
 app.listen(port)
